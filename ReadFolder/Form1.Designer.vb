b34eb7538a504ac7ed6ad8f7d00a0283f12a623d
太郎 総務 Hp <gbrcsoumu001@gmail.com>
2020-10-06 15:14:21 +0900

    プロジェクト ファイルを追加します。

---
+++ ReadFolder/Form1.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(35, 198)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(673, 586)
+        Me.TreeView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(35, 113)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(148, 54)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "ファイル情報読込"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(35, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(148, 49)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "フォルダ選択"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(35, 76)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(673, 22)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(768, 200)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(746, 584)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.WordWrap = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(768, 173)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(746, 22)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(768, 114)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(159, 54)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "テキスト保存"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(208, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 15)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(208, 134)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Label1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(946, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 15)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Label1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MS UI Gothic", 32.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label4.Location = New System.Drawing.Point(771, 29)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(518, 54)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "ファイル情報読取ツール"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(558, 4)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(114, 19)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(558, 29)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(114, 19)
+        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(558, 51)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(114, 19)
+        Me.RadioButton3.TabIndex = 11
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(1346, 134)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 15)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Label5"
+        '
+        'Timer1
+        '
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(1346, 114)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(48, 15)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Label6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(1346, 154)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(48, 15)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Label7"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1538, 796)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ReadFolder ver.2.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+End Class
---
+++ ReadFolder/Form1.vb
@@ -0,0 +1,259 @@
+﻿Imports System
+Imports System.IO
+Imports System.Net
+
+
+Public Class Form1
+    Private filename() As String, fname() As String, dir1() As String
+    Private Path1 As String, Path2 As String
+    Private Result As String, nn As Integer
+    Private ip As String
+    Private Const Nas1 As String = "\\192.168.37.240\fire\防火材料\依頼試験"
+    Private Const Nas2 As String = "\\192.168.37.241\fire"
+    Private Const Nas3 As String = "\\192.168.37.242\fire"
+    Private st As DateTime, ed As DateTime, time1 As TimeSpan
+    Private Count1 As Long
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Using sfd As SaveFileDialog = New SaveFileDialog
+
+            'デフォルトのファイル名を指定します
+            sfd.FileName = TextBox3.Text
+            sfd.InitialDirectory = Path2
+            If sfd.ShowDialog() = DialogResult.OK Then
+                Using sw As StreamWriter = New StreamWriter(sfd.FileName, False, System.Text.Encoding.UTF8)
+                    'ファイルに書き込み
+                    sw.Write(TextBox2.Text)
+                End Using
+            End If
+        End Using
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        TextBox1.Text = Nas1
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label5.Text = Result.Length.ToString
+        Dim ts As TimeSpan = DateTime.Now - st
+        Label6.Text = ts.ToString()
+        Label7.Text = Count1.ToString()
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        TextBox1.Text = Nas2
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        TextBox1.Text = Nas3
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim strHostName As String
+        Dim ipAddr As IPAddress
+
+
+        'ホスト名の取得
+        strHostName = Dns.GetHostName()
+        'ホスト名を表示
+        'MessageBox.Show("ホスト名：" & strHostName, "処理結果")
+
+
+        'IPリストの取得
+        Dim adrList As IPAddress() = Dns.GetHostAddresses(strHostName)
+        'IPリストの最初の値を取得
+        ipAddr = adrList(1)
+        ip = ipAddr.ToString()
+
+        Result = ""
+        'TextBox1.Text = "\\192.168.0.173\disk1\報告書（耐火）＿業務課から\2000Ⅲ耐火防火試験室"
+        If TextBox1.Text = "" Then
+            If ip.IndexOf(".37") = -1 Then
+                TextBox1.Text = "\\192.168.0.173\disk1\報告書（耐火）＿業務課から\2000Ⅲ耐火防火試験室"
+            Else
+                TextBox1.Text = "\\192.168.37.241\fire\"
+            End If
+
+        End If
+        TextBox2.Text = ""
+        Path1 = TextBox1.Text
+        Path2 = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\耐火NAS"
+        Label1.Text = "操作１：ファイル情報を読み取るフォルダを選択する。"
+        Label2.Text = "操作２：ファイル情報を読み取り、表示する"
+        Label3.Text = "操作３：ファイル情報をテキストファイルに保存する。"
+
+        RadioButton1.Text = "NAS1(192.168.37.240)"
+        RadioButton2.Text = "NAS2(192.168.37.241)"
+        RadioButton3.Text = "NAS3(192.168.37.242)"
+
+        Label5.Text = "0"
+        Label6.Text = "0"
+        Label7.Text = "0"
+        Timer1.Interval = 1000
+        Timer1.Enabled = False
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Dim sFolder As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        'Result = ""
+        st = DateTime.Now
+        Timer1.Enabled = True
+        Result = TextBox1.Text + vbCrLf + vbCrLf
+        TextBox2.Text = ""
+        TreeView1.ShowNodeToolTips = True
+        TreeView1.BeginUpdate()
+        TreeView1.Nodes.Clear()
+        nn = 0
+        AddNode(TreeView1.Nodes, Path1)
+        TreeView1.EndUpdate()
+        TextBox2.Text = Result
+        ed = DateAndTime.Now
+        time1 = ed - st
+        TextBox2.Text += vbCrLf + time1.ToString + vbCrLf
+        Timer1.Enabled = False
+        Count1 = 0
+
+        Dim i As Integer = 0
+        Do
+            i += 1
+            If Path1.Substring(Path1.Length - i, 1) <> "　" And Path1.Substring(Path1.Length - i, 1) <> " " Then Exit Do
+        Loop
+        i -= 1
+        If i > 0 Then
+            Dim b As String = Path1.Substring(0, Path1.Length - i)
+            Path1 = b
+        End If
+        'TextBox3.Text = Path.GetFileName(Path1) + ".txt"
+        TextBox3.Text = Path1.Replace("\", "-") + ".txt"
+
+    End Sub
+
+    Sub AddNode(ByVal Nodes As TreeNodeCollection, ByVal sFolder As String)
+        Dim N = Nodes.Add(System.IO.Path.GetFileName(sFolder))
+        Dim Level As Integer = N.Level
+        Dim tab1 As String
+        Dim s As String
+        If Level > 0 Then
+            tab1 = New String(vbTab, Level) + "・ "
+        Else
+            tab1 = ""
+        End If
+        'Result += Path.GetFileName(Path.GetDirectoryName(sFolder)) + vbCrLf
+        'Result += tab1 + Path.GetFileName(sFolder) + " (dir)" + vbCrLf
+        'nn += 1
+        'Try
+        Dim i As Integer = 0
+        Do
+            i += 1
+            If sFolder.Substring(sFolder.Length - i, 1) <> "　" And sFolder.Substring(sFolder.Length - i, 1) <> " " Then Exit Do
+        Loop
+        i -= 1
+        'Dim a As String = sFolder.Substring(sFolder.Length - 1, 1)
+        If i > 0 Then
+            Dim b As String = sFolder.Substring(0, sFolder.Length - i)
+            Dim c As String = sFolder + "a"
+            System.IO.Directory.Move(sFolder, c)
+            System.IO.Directory.Move(c, b)
+            sFolder = b
+        End If
+        Result += tab1 + Path.GetFileName(sFolder) + " (dir)" + vbCrLf
+        Count1 += 1
+        's = sFolder + "\"
+        'Directory.SetCurrentDirectory(s)
+        'Dim dirpath As String = Directory.GetCurrentDirectory()
+        Try
+            For Each sName In My.Computer.FileSystem.GetDirectories(sFolder)
+                'For Each sName In My.Computer.FileSystem.GetDirectories(Environment.CurrentDirectory)
+                'For Each sName In Environment.CurrentDirectory.
+                Application.DoEvents()
+                AddNode(N.Nodes, sName)
+                'Result += Path.GetFileName(Path.GetDirectoryName(sName)) + vbCrLf
+            Next
+        Catch ex As UnauthorizedAccessException
+            N.ToolTipText = ex.Message
+            N.ForeColor = Color.Red
+            'Result += Path.GetFileName(Path.GetDirectoryName(sFolder)) + vbCrLf
+            'nn -= 1
+        End Try
+        Try
+            'Result += Path.GetFileName(Path.GetDirectoryName(sFolder)) + vbCrLf
+
+            For Each sName In My.Computer.FileSystem.GetFiles(sFolder)
+                Dim fname As String = Path.GetFileName(sName)
+                'Result += fname + vbCrLf
+                'nn += 1
+                N.Nodes.Add(fname)
+                Level = N.Level
+                'If Level > 0 Then
+                tab1 = New String(vbTab, Level + 1) + "・ "
+                'Else
+                'tab1 = ""
+                'End If
+                Result += tab1 + fname + " (file)" + vbCrLf
+                Count1 += 1
+            Next
+        Catch ex As UnauthorizedAccessException
+            N.ToolTipText = ex.Message
+            N.ForeColor = Color.Red
+            'Result += Path.GetFileName(Path.GetDirectoryName(sFolder)) + vbCrLf
+            'nn -= 1
+        End Try
+        N.EnsureVisible()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim fbd As New FolderBrowserDialog
+
+        '上部に表示する説明テキストを指定する
+        fbd.Description = "読み込むフォルダを指定してください。"
+        'ルートフォルダを指定する
+        'デフォルトでDesktop
+        fbd.RootFolder = Environment.SpecialFolder.Desktop
+        '最初に選択するフォルダを指定する
+        'RootFolder以下にあるフォルダである必要がある
+        'fbd.SelectedPath = "\\192.168.0.173\disk1\報告書（耐火）＿業務課から"
+        If TextBox1.Text <> "" Then
+            fbd.SelectedPath = TextBox1.Text
+        Else
+            fbd.SelectedPath = "\\192.168.37.241\fire\"
+        End If
+
+        'ユーザーが新しいフォルダを作成できるようにする
+        'デフォルトでTrue
+        fbd.ShowNewFolderButton = True
+
+        'ダイアログを表示する
+        If fbd.ShowDialog(Me) = DialogResult.OK Then
+            '選択されたフォルダを表示する
+            Me.TextBox1.Text = fbd.SelectedPath
+            Path1 = fbd.SelectedPath
+            ''Dim filename() As String, fname() As String, dir1() As String
+
+            'Try
+            '    filename = System.IO.Directory.GetFiles _
+            '    (Path1, "*.xdw", System.IO.SearchOption.AllDirectories)
+            '    Dim n As Integer = filename.Length
+            '    Me.TextBox3.Text = ""
+
+            '    ReDim fname(n - 1), dir1(n - 1)
+
+            '    For i As Integer = 0 To n - 1
+            '        fname(i) = System.IO.Path.GetFileNameWithoutExtension(filename(i))
+            '        dir1(i) = System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(filename(i)))
+            '        Me.TextBox3.Text += filename(i) + vbCrLf
+            '        Me.TextBox3.SelectionStart = Me.TextBox3.Text.Length
+            '        Me.TextBox3.Focus()
+            '        Me.TextBox3.ScrollToCaret()
+            '        'Console.WriteLine(a)
+
+            '    Next
+            'Catch e1 As Exception
+            '    'Console.WriteLine(e1.Message)
+            'End Try
+        End If
+    End Sub
+End Class
